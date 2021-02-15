f2ea26073409211c1a16687aaf5f692b35de3c5b
srikanth kamparaju <47571970+srikanth2001@users.noreply.github.com>
2021-02-15 17:35:15 +0530

    Add files via upload

--- Assignment-01/codes/IFFT_matrix.py
+++ Assignment-01/codes/IFFT_matrix.py
@@ -1,17 +1,10 @@
 import numpy as np 
 
+
 Y = np.array([16.65625,-2.95312+1j*1.16372,-0.07813+1j*1.1096,-3.84375,-0.07813-1j*1.1096,-2.95312-1j*1.16372]) 
-def ifft_matrix(n):
-	fft_mat = np.zeros((n,n),dtype=np.complex128)
-	for i in range(n):
+fft_mat = np.zeros((len(Y),len(Y)),dtype=np.complex128)
+n = len(Y)
+for i in range(n):
 		for j in range(n):
 				fft_mat[i][j] = (1/n)*np.exp(2j*np.pi*i*j/n)
-	return fft_mat
-
-def ifft(x):
-	n = len(x)
-	F = ifft_matrix(n)
-	return F@x
-
-
-print(ifft(Y))
+print(fft_mat@Y)
